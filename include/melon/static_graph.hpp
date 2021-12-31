3af0310613a2474f27f05e27f9ef109d6018a7cf
PlaiSeek <francois.hamonic@gmail.com>
2021-12-31 15:11:06 +0100

    static graph constructor with concepts

--- include/melon/static_graph.hpp
+++ include/melon/static_graph.hpp
@@ -11,15 +11,42 @@ class StaticDigraph {
 public:
     using Node = std::size_t;
     using Arc = std::size_t;
+    using ArcList = std::vector<std::pair<Node, Node>>;
 
 public:
     std::vector<Arc> out_arc_begin;
     std::vector<Node> arc_target;
 
 public:
-    // todo bulk constructor : rvalue ranges::random_access_range
-    StaticDigraph(std::vector<Arc> && begins, std::vector<Node> && targets)
-        : out_arc_begin(std::move(begins)), arc_target(std::move(targets)) {}
+    template <std::ranges::random_access_range Arcs>
+    requires(std::is_rvalue_reference<Arcs &&>::value)
+        StaticDigraph(const std::size_t nb_nodes, Arcs && arcs)
+        : out_arc_begin(nb_nodes), arc_target(arcs.size()) {
+        // sort arcs
+        std::ranges::sort(arcs, [](const auto & a, const auto & b) {
+            if(a.first == b.first) return a.second < b.second;
+            return a.first < b.first;
+        });
+        // fill out_arc_begin
+        std::size_t u = 0;
+        out_arc_begin[0] = 0;
+        for(auto it = arcs.begin(); it != arcs.end(); ++it) {
+            if(u == it->first) continue;
+            std::fill(out_arc_begin.begin() + u + 1,
+                      out_arc_begin.begin() + it->first + 1,
+                      std::distance(arcs.begin(), it));
+            u = it->first;
+        }
+        std::fill(out_arc_begin.begin() + u + 1, out_arc_begin.end(),
+                  arcs.size());
+        // fill arc_target
+        std::ranges::copy(std::views::values(arcs), arc_target.begin());
+    }
+    template <std::ranges::range Arcs>
+    StaticDigraph(const std::size_t nb_nodes, Arcs && arcs)
+        : StaticDigraph(nb_nodes, ArcList(arcs)) {
+            std::cout << "makes copy" << std::endl;
+        }
 
     std::size_t nb_nodes() const { return out_arc_begin.size(); }
     std::size_t nb_arcs() const { return arc_target.size(); }
@@ -31,18 +58,18 @@ public:
         return std::views::iota(static_cast<std::size_t>(0), nb_arcs());
     }
 
-    auto out_arcs(Node u) const {
+    auto out_arcs(const Node u) const {
         return std::views::iota(
             out_arc_begin[u],
             (u + 1 < nb_nodes() ? out_arc_begin[u + 1] : nb_arcs()));
     }
-    auto out_neighbors(Node u) const {
+    auto out_neighbors(const Node u) const {
         return std::ranges::subrange(
             arc_target.begin() + out_arc_begin[u],
             (u + 1 < nb_nodes() ? arc_target.begin() + out_arc_begin[u + 1]
                                 : arc_target.end()));
     }
-    auto out_arcs_pairs(Node u) const {
+    auto out_arcs_pairs(const Node u) const {
         return std::views::transform(
             out_neighbors(u), [u](auto v) { return std::make_pair(u, v); });
     }
@@ -52,48 +79,6 @@ public:
     }
 };
 
-class StaticDigraphBuilder {
-public:
-    using Node = StaticDigraph::Node;
-    using Arc = StaticDigraph::Arc;
-
-private:
-    std::size_t nb_nodes;
-    std::vector<std::pair<Node, Node>> arcs;
-
-public:
-    StaticDigraphBuilder() {}
-
-    StaticDigraph build() {
-        std::ranges::sort(arcs, [](const auto & a, const auto & b) {
-            if(a.first == b.first) return a.second < b.second;
-            return a.first < b.first;
-        });
-
-        std::vector<Arc> begins(nb_nodes);
-        std::vector<Node> targets(arcs.size());
-
-        // fill begins
-        std::size_t u = 0;
-        begins[0] = 0;
-        for(auto it = arcs.begin(); it != arcs.end(); ++it) {
-            if(u == it->first) continue;
-            std::fill(begins.begin() + u + 1, begins.begin() + it->first + 1,
-                      std::distance(arcs.begin(), it));
-            u = it->first;
-        }
-        std::fill(begins.begin() + u + 1, begins.end(), arcs.size());
-        // fill targets
-        std::ranges::copy(std::views::values(arcs), targets.begin());
-
-        StaticDigraph graph(std::move(begins), std::move(targets));
-        return graph;
-    }
-
-    void setNbNodes(std::size_t n) { nb_nodes = n; }
-    void add(Node u, Node v) { arcs.emplace_back(u, v); }
-};
-
-}  // namespace lemon
+}  // namespace melon
 
 #endif  // STATIC_GRAPH_HPP
