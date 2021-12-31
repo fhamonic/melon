87be6be9b2de4367375a6d416f5b6ef13a80eeeb
PlaiSeek <francois.hamonic@gmail.com>
2021-12-31 15:11:06 +0100

    static graph constructor with concepts

--- Makefile
+++ Makefile
@@ -14,27 +14,17 @@ $(BUILD_DIR):
 	@mkdir $(BUILD_DIR) && \
 	cd $(BUILD_DIR) && \
 	conan install .. && \
-	cmake -DCMAKE_CXX_COMPILER=g++-10 -DCMAKE_BUILD_TYPE=Release -DWARNINGS=ON -DHARDCORE_WARNINGS=ON -DCOMPILE_FOR_NATIVE=ON -DCOMPILE_WITH_LTO=ON ..
+	cmake -DCMAKE_CXX_COMPILER=g++-10 -DCMAKE_BUILD_TYPE=Debug -DWARNINGS=ON -DHARDCORE_WARNINGS=ON -DCOMPILE_FOR_NATIVE=ON -DCOMPILE_WITH_LTO=ON ..
 
 clean:
 	@rm -rf $(BUILD_DIR)
 
-single-header: single-header/milppp_cbc.hpp single-header/milppp_grb.hpp
-
-single-header/milppp_cbc.hpp:
-	@python3 -m quom --include_directory include include/milppp_cbc.hpp milppp_cbc.hpp.tmp && \
+single-header: 
+	@python3 -m quom --include_directory include include/all.hpp melon.hpp.tmp && \
 	mkdir -p single-header && \
-	echo "/*" > single-header/milppp_cbc.hpp && \
-	cat LICENSE >> single-header/milppp_cbc.hpp && \
-	echo "*/" >> single-header/milppp_cbc.hpp && \
-	cat milppp_cbc.hpp.tmp >> single-header/milppp_cbc.hpp && \
-	rm milppp_cbc.hpp.tmp
+	echo "/*" > single-header/melon.hpp && \
+	cat LICENSE >> single-header/melon.hpp && \
+	echo "*/" >> single-header/melon.hpp && \
+	cat melon.hpp.tmp >> single-header/melon.hpp && \
+	rm melon.hpp.tmp
 
-single-header/milppp_grb.hpp:
-	@python3 -m quom --include_directory include include/milppp_grb.hpp milppp_grb.hpp.tmp && \
-	mkdir -p single-header && \
-	echo "/*" > single-header/milppp_grb.hpp && \
-	cat LICENSE >> single-header/milppp_grb.hpp && \
-	echo "*/" >> single-header/milppp_grb.hpp && \
-	cat milppp_grb.hpp.tmp >> single-header/milppp_grb.hpp && \
-	rm milppp_grb.hpp.tmp
--- test/main.cpp
+++ test/main.cpp
@@ -1,54 +1,56 @@
-#include <gtest/gtest.h>
 #include <iostream>
 
 #include "melon/static_graph.hpp"
 
-#include "lemon/static_graph.h"
-#include "lemon/smart_graph.h"
+// #include "lemon/static_graph.h"
+// #include "lemon/smart_graph.h"
+
+auto fill_arcs() {
+    melon::StaticDigraph::ArcList arcs;
+    arcs.emplace_back(1,2);
+    arcs.emplace_back(1,6);
+    arcs.emplace_back(1,7);
+    arcs.emplace_back(2,3);
+    arcs.emplace_back(2,4);
+    arcs.emplace_back(3,4);
+    arcs.emplace_back(5,2);
+    arcs.emplace_back(5,3);
+    arcs.emplace_back(6,5);
+    return arcs;
+}
 
 int main(int argc, char ** argv) {
-    StaticDigraphBuilder builder;
+    const melon::StaticDigraph::ArcList arcs = fill_arcs();
 
-    builder.setNbNodes(8);
-    builder.add(1,2);
-    builder.add(1,6);
-    builder.add(1,7);
-    builder.add(2,3);
-    builder.add(2,4);
-    builder.add(3,4);
-    builder.add(5,2);
-    builder.add(5,3);
-    builder.add(6,5);
+    melon::StaticDigraph graph(8, arcs);
 
-    StaticDigraph graph = builder.build();
-
-    for(const auto u : graph.nodes()) {
-        std::cout << u << std::endl;
-    }
-    for(const auto a : graph.arcs()) {
-        std::cout << a << std::endl;
-    }
+    // for(const auto u : graph.nodes()) {
+    //     std::cout << u << std::endl;
+    // }
+    // for(const auto a : graph.arcs()) {
+    //     std::cout << a << std::endl;
+    // }
 
 
-    std::cout << "out begins: " << std::endl;
-    for(const auto & begin : graph.out_arc_begin) {
-        std::cout << begin << std::endl;
-    }
-    std::cout << "out targets: " << std::endl;
-    for(const auto & target : graph.arc_target) {
-        std::cout << target << std::endl;
-    }
+    // std::cout << "out begins: " << std::endl;
+    // for(const auto & begin : graph.out_arc_begin) {
+    //     std::cout << begin << std::endl;
+    // }
+    // std::cout << "out targets: " << std::endl;
+    // for(const auto & target : graph.arc_target) {
+    //     std::cout << target << std::endl;
+    // }
 
 
 
-    std::cout << "out neighbors of 1: " << std::endl;
-    for(const auto & a : graph.out_neighbors(1)) {
-        std::cout << a << std::endl;
-    }
-    std::cout << "pairs: " << std::endl;
-    for(const auto & [u,v] : graph.arcs_pairs()) {
-        std::cout << u << " " << v << std::endl;
-    }
+    // std::cout << "out neighbors of 1: " << std::endl;
+    // for(const auto & a : graph.out_neighbors(1)) {
+    //     std::cout << a << std::endl;
+    // }
+    // std::cout << "pairs: " << std::endl;
+    // for(const auto & [u,v] : graph.arcs_pairs()) {
+    //     std::cout << u << " " << v << std::endl;
+    // }
 
     return 0;
 }
