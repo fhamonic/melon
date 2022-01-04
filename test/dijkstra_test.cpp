a83648da51f864f7d7c5e143d2bcd09358d09e16
PlaiSeek <francois.hamonic@gmail.com>
2022-01-05 00:56:20 +0100

    dijkstra tests ok

--- test/dijkstra_test.cpp
+++ test/dijkstra_test.cpp
@@ -1,22 +1,13 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
 #include "melon/dijkstra.hpp"
-#include "melon/static_digraph.hpp"
 #include "melon/static_digraph_builder.hpp"
 
-using namespace fhamonic;
-
-// #include "lemon/static_graph.h"
-// #include "lemon/smart_graph.h"
+#include "ranges_test_helper.hpp"
 
 using namespace fhamonic::melon;
 
-template <typename T1, typename T2>
-std::ostream & operator<<(std::ostream & os, const std::pair<T1, T2> & p) {
-    return os << '(' << p.first << ',' << p.second << ')';
-}
-
-int main() {
+GTEST_TEST(Dijkstra, test) {
     StaticDigraphBuilder<int> builder(6);
 
     builder.addArc(0, 1, 7);
@@ -43,7 +34,17 @@ int main() {
     Dijkstra dijkstra(graph, length_map);
 
     dijkstra.init(0);
-    while(!dijkstra.emptyQueue()) dijkstra.processNextNode();
-
-    return 0;
+    ASSERT_FALSE(dijkstra.emptyQueue());
+    ASSERT_EQ(dijkstra.processNextNode(), std::make_pair(0u, 0));
+    ASSERT_FALSE(dijkstra.emptyQueue());
+    ASSERT_EQ(dijkstra.processNextNode(), std::make_pair(1u, 7));
+    ASSERT_FALSE(dijkstra.emptyQueue());
+    ASSERT_EQ(dijkstra.processNextNode(), std::make_pair(2u, 9));
+    ASSERT_FALSE(dijkstra.emptyQueue());
+    ASSERT_EQ(dijkstra.processNextNode(), std::make_pair(5u, 11));
+    ASSERT_FALSE(dijkstra.emptyQueue());
+    ASSERT_EQ(dijkstra.processNextNode(), std::make_pair(4u, 20));
+    ASSERT_FALSE(dijkstra.emptyQueue());
+    ASSERT_EQ(dijkstra.processNextNode(), std::make_pair(3u, 21));
+    ASSERT_TRUE(dijkstra.emptyQueue());
 }
