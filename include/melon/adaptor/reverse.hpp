1f0b628d8c8c9011c6b78497a8a990cd10aed968
PlaiSeek <francois.hamonic@gmail.com>
2022-04-17 19:10:27 +0200

    default initializable no more mandatory, reverse adaptor ok

--- include/melon/adaptor/reverse.hpp
+++ include/melon/adaptor/reverse.hpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <functional>
 #include <ranges>
 #include <vector>
 
@@ -24,7 +25,7 @@ public:
     using arc_map = typename G::arc_map<T>;
 
 private:
-    const G & _graph;
+    std::reference_wrapper<const G> _graph;
 
 public:
     reverse(const G & g) : _graph(g) {}
@@ -36,38 +37,47 @@ public:
     reverse & operator=(reverse &&) = default;
 
     auto nb_vertices() const requires requires(const G & g) { g.nb_vertices(); }
-    { return _graph.nb_vertices(); }
+    { return _graph.get().nb_vertices(); }
     auto nb_arcs() const noexcept requires requires(const G & g) {
         g.nb_arcs();
     }
-    { return _graph.nb_arcs(); }
+    { return _graph.get().nb_arcs(); }
 
-    auto vertices() const noexcept { return _graph.vertices(); }
-    auto arcs() const noexcept { return _graph.arcs(); }
+    auto vertices() const noexcept { return _graph.get().vertices(); }
+    auto arcs() const noexcept { return _graph.get().arcs(); }
 
-    vertex_t source(arc_t a) const noexcept requires concepts::has_arc_target<G> {
-        return _graph.target(a);
+    vertex_t source(
+        arc_t a) const noexcept requires concepts::has_arc_target<G> {
+        return _graph.get().target(a);
     }
-    vertex_t target(arc_t a) const noexcept requires concepts::has_arc_source<G> {
-        return _graph.source(a);
+    vertex_t target(
+        arc_t a) const noexcept requires concepts::has_arc_source<G> {
+        return _graph.get().source(a);
     }
 
     auto out_arcs(const vertex_t u)
         const noexcept requires concepts::reversible_incidence_list_graph<G> {
-        return _graph.in_arcs(u);
+        return _graph.get().in_arcs(u);
     }
     auto in_arcs(const vertex_t u)
         const noexcept requires concepts::incidence_list_graph<G> {
-        return _graph.out_arcs(u);
+        return _graph.get().out_arcs(u);
     }
 
     auto out_neighbors(const vertex_t u)
         const noexcept requires concepts::reversible_adjacency_list_graph<G> {
-        return _graph.in_neighbors(u);
+        return _graph.get().in_neighbors(u);
     }
     auto in_neighbors(const vertex_t u)
         const noexcept requires concepts::adjacency_list_graph<G> {
-        return _graph.out_neighbors(u);
+        return _graph.get().out_neighbors(u);
+    }
+
+    auto arcs_pairs()
+        const noexcept requires concepts::adjacency_list_graph<G> {
+        return std::views::transform(_graph.get().arcs_pairs(), [](auto && p) {
+            return std::make_pair(p.second, p.first);
+        });
     }
 };
 
