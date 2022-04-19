12cf87a0e8cf9bd656d9a35e41223bdd54998be6
= <francois.hamonic@gmail.com>
2022-04-19 14:27:47 +0200

    robust fiber : todo find an initialization method

--- include/melon/algorithm/robust_fiber.hpp
+++ include/melon/algorithm/robust_fiber.hpp
@@ -12,6 +12,7 @@
 #include "melon/concepts/graph_concepts.hpp"
 #include "melon/data_structures/d_ary_heap.hpp"
 #include "melon/data_structures/fast_binary_heap.hpp"
+#include "melon/utils/prefetch.hpp"
 #include "melon/utils/traversal_algorithm_behavior.hpp"
 #include "melon/utils/traversal_algorithm_iterator.hpp"
 
@@ -19,24 +20,24 @@ namespace fhamonic {
 namespace melon {
 
 template <concepts::adjacency_list_graph GR, typename LM1, typename LM2,
-          bool strictly_strong = false, typename F1, typename F2,
+          typename F1, typename F2, bool strictly_strong = false,
           typename SR = DijkstraShortestPathSemiring<typename LM1::value_type>>
 class RobustFiber {
 public:
     using vertex_t = GR::vertex_t;
     using arc_t = GR::arc_t;
 
-    using Value = LM::value_type;
+    using Value = LM1::value_type;
     using DijkstraSemiringTraits = SR;
 
     struct Entry {
         Value dist;
         bool strong;
 
-        Entry operator+(Value v) {
+        Entry operator+(Value v) const noexcept {
             return Entry(DijkstraSemiringTraits::plus(dist, v), strong);
         }
-        bool operator==(const Entry & o) {
+        bool operator==(const Entry & o) const noexcept {
             return dist == o.dist && strong == o.strong;
         }
     };
@@ -57,8 +58,8 @@ public:
 
 private:
     const GR & _graph;
-    const LM1 & _length_map;
-    const LM2 & _reduced_length_map;
+    const LM1 & _reduced_length_map;
+    const LM2 & _length_map;
 
     F1 _callback_strong;
     F2 _callback_weak;
@@ -72,8 +73,8 @@ public:
     RobustFiber(const GR & g, const LM1 & l1, const LM2 & l2, F1 && f1,
                 F2 && f2)
         : _graph(g)
-        , _length_map(l1)
-        , _reduced_length_map(l2)
+        , _reduced_length_map(l1)
+        , _length_map(l2)
         , _callback_strong(std::forward<F1>(f1))
         , _callback_weak(std::forward<F2>(f2))
         , _heap(g.nb_vertices())
@@ -86,26 +87,25 @@ public:
         nb_weak_candidates = 0;
         return *this;
     }
-    RobustFiber & add_source(vertex_t s,
-                             Value dist = DijkstraSemiringTraits::zero,
-                             bool strong = false) noexcept {
+    RobustFiber & add_fiber_arc(vertex_t s, arc_t st,
+                             Value dist = DijkstraSemiringTraits::zero) noexcept {
         assert(_heap.state(s) != Heap::IN_HEAP);
-        if(strong) {
-            ++nb_strong_candidates;
-        } else {
-            ++nb_weak_candidates;
-        }
         _heap.push(s, Entry(dist, strong));
+            ++nb_strong_candidates;
         return *this;
     }
-    RobustFiber & add_definitive_source(vertex_t s,
-                             Value dist = DijkstraSemiringTraits::zero,
-                             bool strong = false) noexcept {
+
+
+
+    RobustFiber & add_definitive_source(
+        vertex_t s, Value dist = DijkstraSemiringTraits::zero,
+        bool strong = false) noexcept {
         assert(_heap.state(s) != Heap::IN_HEAP);
-        if(e.strong) {
-           process_strong_node(s, Entry(dist, strong));
+        _heap.discard(s);
+        if(strong) {
+            process_strong_node(s, Entry(dist, strong));
         } else {
-           process_strong_node(s, Entry(dist, strong));
+            process_weak_node(s, Entry(dist, strong));
         }
         return *this;
     }
@@ -155,33 +155,32 @@ public:
     void run() noexcept {
         while(nb_strong_candidates > 0 && nb_weak_candidates > 0) {
             const auto && [u, entry] = _heap.top();
-            prefetch_range(_graph.out_arcs(p.first));
-            prefetch_range(_graph.out_neighbors(p.first));
-            if(p.second.strong) {
-                prefetch_map_values(_graph.out_arcs(p.first), _length_map);
-                _callback_strong(p.first);
+            prefetch_range(_graph.out_arcs(u));
+            prefetch_range(_graph.out_neighbors(u));
+            if(entry.strong) {
+                prefetch_map_values(_graph.out_arcs(u), _length_map);
+                _callback_strong(u);
                 _heap.pop();
                 --nb_strong_candidates;
-                process_strong_node(p);
+                process_strong_node(u, entry);
             } else {
-                prefetch_map_values(_graph.out_arcs(p.first),
-                                    _reduced_length_map);
-                _callback_weak(p.first);
+                prefetch_map_values(_graph.out_arcs(u), _reduced_length_map);
+                _callback_weak(u);
                 _heap.pop();
                 --nb_weak_candidates;
-                process_weak_node(p);
+                process_weak_node(u, entry);
             }
         }
         if(nb_strong_candidates > 0) {
             for(auto && v : _graph.vertices()) {
-                if(_heap.status(v) == Heap::POST_HEAP) continue;
+                if(_heap.state(v) == Heap::POST_HEAP) continue;
                 _callback_strong(v);
             }
             return;
         }
         if(nb_weak_candidates > 0) {
-            for(auto && [v, prio] : _heap.entries()) {
-                if(_heap.status(v) == Heap::POST_HEAP) continue;
+            for(auto && v : _graph.vertices()) {
+                if(_heap.state(v) == Heap::POST_HEAP) continue;
                 _callback_weak(v);
             }
         }
