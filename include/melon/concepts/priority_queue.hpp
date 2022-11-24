fdb7adc4efbacf8f3140a549b04042be91cb00c1
fhamonic <francois.hamonic@gmail.com>
2022-11-24 03:05:42 +0100

    ok

--- include/melon/concepts/priority_queue.hpp
+++ include/melon/concepts/priority_queue.hpp
@@ -27,8 +27,8 @@ concept updatable_priority_queue = priority_queue<Q> &&
     requires(Q q, typename Q::key_type k, typename Q::priority_type v) {
     { q.contains(k) } -> std::convertible_to<bool>;
     { q.priority(k) } -> std::same_as<typename Q::priority_type>;
-    // q.demote(k, v);
     q.promote(k, v);
+    // q.demote(k, v);
     // q.update(k, v);
 };
 }  // namespace concepts
