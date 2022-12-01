ed33e74f3f5975e4ecbf583dd77c735ac27578eb
fhamonic <francois.hamonic@gmail.com>
2022-12-01 22:11:38 +0100

    map_op concepts

--- include/melon/concepts/map_of.hpp
+++ include/melon/concepts/map_of.hpp
@@ -13,12 +13,10 @@ using mapped_value_t =
 
 // clang-format off
 namespace concepts {
-
 template <typename M, typename K>
 concept input_map = requires(M m, K k) {
     m[k];
 };
-
 template <typename M, typename K, typename V>
 concept input_map_of = input_map<M, K> && std::convertible_to<mapped_value_t<M,K>,V>;
 
@@ -27,7 +25,6 @@ concept output_map = input_map<M, K> &&
     requires(M map, K key, mapped_value_t<M,K> value) {
         { map[key] = value } -> std::convertible_to<mapped_value_t<M,K>>;
     };
-
 template <typename M, typename K, typename V>
 concept output_map_of = output_map<M, K> && std::convertible_to<mapped_value_t<M,K>,V>;
 
