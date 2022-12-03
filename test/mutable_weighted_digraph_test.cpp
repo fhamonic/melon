d28bceb6c64c4a9b48d3d5059e2c51d9990d3793
fhamonic <francois.hamonic@gmail.com>
2022-12-03 03:52:53 +0100

    mutable weighted digraph

--- test/mutable_weighted_digraph_test.cpp
+++ test/mutable_weighted_digraph_test.cpp
@@ -9,8 +9,8 @@ using namespace fhamonic;
 using namespace fhamonic::melon;
 
 static_assert(melon::concepts::graph<mutable_weighted_digraph<int>>);
-static_assert(melon::concepts::incidence_list_graph<
-              mutable_weighted_digraph<int>>);
+static_assert(
+    melon::concepts::incidence_list_graph<mutable_weighted_digraph<int>>);
 // static_assert(melon::concepts::adjacency_list_graph<
 //               mutable_weighted_digraph<int>>);
 // static_assert(
@@ -42,40 +42,40 @@ GTEST_TEST(mutable_weighted_digraph, create_vertices) {
     ASSERT_EQ_RANGES(graph.vertices(), {0, 1, 2});
 
     ASSERT_EQ(std::ranges::distance(graph.out_arcs(0)), 0);
-    // ASSERT_EQ(std::ranges::distance(graph.out_arcs(1)), 0);
-    // ASSERT_EQ(std::ranges::distance(graph.out_arcs(2)), 0);
+    ASSERT_EQ(std::ranges::distance(graph.out_arcs(1)), 0);
+    ASSERT_EQ(std::ranges::distance(graph.out_arcs(2)), 0);
     ASSERT_FALSE(graph.is_valid_node(3));
-    // EXPECT_DEATH(graph.out_arcs(3), "");
+    EXPECT_DEATH(graph.out_arcs(3), "");
 }
 
-// GTEST_TEST(mutable_weighted_digraph, create_arcs) {
-//     using Graph = mutable_weighted_digraph<double>;
-//     Graph graph;
+GTEST_TEST(mutable_weighted_digraph, create_arcs) {
+    using Graph = mutable_weighted_digraph<double>;
+    Graph graph;
 
-//     auto a = graph.create_vertex();
-//     auto b = graph.create_vertex();
-//     auto c = graph.create_vertex();
+    auto a = graph.create_vertex();
+    auto b = graph.create_vertex();
+    auto c = graph.create_vertex();
 
-//     auto ab = graph.create_arc(a, b, 0.2);
-//     auto ac = graph.create_arc(a, c, 1.0);
-//     auto cb = graph.create_arc(c, b, 3.14);
+    auto ab = graph.create_arc(a, b, 0.2);
+    auto ac = graph.create_arc(a, c, 1.0);
+    auto cb = graph.create_arc(c, b, 3.14);
 
-//     ASSERT_EQ(graph.target(ab), b);
-//     ASSERT_EQ(graph.target(ac), c);
-//     ASSERT_EQ(graph.target(cb), b);
+    ASSERT_EQ(graph.target(ab), b);
+    ASSERT_EQ(graph.target(ac), c);
+    ASSERT_EQ(graph.target(cb), b);
 
-//     ASSERT_EQ(graph.weight(ab), 0.2);
-//     ASSERT_EQ(graph.weight(ac), 1.0);
-//     ASSERT_EQ(graph.weight(cb), 3.14);
+    ASSERT_EQ(graph.weight(ab), 0.2);
+    ASSERT_EQ(graph.weight(ac), 1.0);
+    ASSERT_EQ(graph.weight(cb), 3.14);
 
-//     std::vector<std::pair<vertex_t<Graph>, vertex_t<Graph>>> pairs = {
-//         {a, b}, {a, c}, {c, b}};
-//     ASSERT_EQ_RANGES(graph.arcs_pairs(), pairs);
+    std::vector<std::pair<vertex_t<Graph>, vertex_t<Graph>>> pairs = {
+        {a, b}, {a, c}, {c, b}};
+    ASSERT_EQ_RANGES(graph.arcs_pairs(), pairs);
 
-    // ASSERT_EQ_RANGES(graph.out_neighbors(a), {c, b});
-    // ASSERT_TRUE(std::ranges::empty(graph.out_neighbors(b)));
-    // ASSERT_EQ_RANGES(graph.out_neighbors(c), {b});
-// }
+    ASSERT_EQ_RANGES(graph.out_neighbors(a), {c, b});
+    ASSERT_EQ(std::ranges::distance(graph.out_neighbors(b)), 0);
+    ASSERT_EQ_RANGES(graph.out_neighbors(c), {b});
+}
 
 // GTEST_TEST(mutable_weighted_digraph, remove_arcs) {
 //     using Graph = mutable_weighted_digraph<double>;
@@ -95,7 +95,8 @@ GTEST_TEST(mutable_weighted_digraph, create_vertices) {
 //     ASSERT_EQ(graph.weight(ab), 0.2);
 //     ASSERT_EQ(graph.weight(cb), 3.14);
 //     std::vector<std::pair<vertex_t<Graph>, vertex_t<Graph>>> pairs = {{a, b},
-//                                                                       {c, b}};
+//                                                                       {c,
+//                                                                       b}};
 //     ASSERT_EQ_RANGES(graph.arcs_pairs(), pairs);
 
 //     ASSERT_EQ_RANGES(graph.out_neighbors(a), {b});
