2af350b718e99f8e855072f0a315cb710fa9c618
fhamonic <francois.hamonic@gmail.com>
2022-12-03 18:18:18 +0100

    mutable_weighted_graph

--- include/melon/mutable_weighted_digraph.hpp
+++ include/melon/mutable_weighted_digraph.hpp
@@ -20,7 +20,9 @@ public:
     struct arc {
         vertex source;
         vertex target;
+        int prev_in_arc;
         int next_in_arc;
+        int prev_out_arc;
         int next_out_arc;
         W weight;
     };
@@ -103,33 +105,96 @@ public:
         _first_out_arc.emplace_back(-1);
         return static_cast<vertex>(_first_out_arc.size() - 1);
     }
+
+private:
+    int index_of_arc(const arc & a) const noexcept {
+        if(a.prev_out_arc != -1) return _arcs[a.prev_out_arc].next_out_arc;
+        return _first_out_arc[a.source];
+    }
+    void remove_from_source_out_arcs(const arc & a) noexcept {
+        if(a.prev_out_arc == -1)
+            _first_out_arc[a.source] = a.next_out_arc;
+        else
+            _arcs[a.prev_out_arc].next_out_arc = a.next_out_arc;
+        if(a.next_out_arc != -1)
+            _arcs[a.next_out_arc].prev_out_arc = a.prev_out_arc;
+    }
+    void remove_from_target_in_arcs(const arc & a) noexcept {
+        if(a.prev_in_arc == -1)
+            _first_in_arc[a.target] = a.next_in_arc;
+        else
+            _arcs[a.prev_in_arc].next_in_arc = a.next_in_arc;
+        if(a.next_in_arc != -1)
+            _arcs[a.next_in_arc].prev_in_arc = a.prev_in_arc;
+    }
+    void change_arc_index(arc & a, const int i) noexcept {
+        if(a.prev_out_arc == -1)
+            _first_out_arc[a.source] = i;
+        else
+            _arcs[a.prev_out_arc].next_out_arc = i;
+        if(a.next_out_arc != -1) _arcs[a.next_out_arc].prev_out_arc = i;
+        if(a.prev_in_arc == -1)
+            _first_in_arc[a.target] = i;
+        else
+            _arcs[a.prev_in_arc].next_in_arc = i;
+        if(a.next_in_arc != -1) _arcs[a.next_in_arc].prev_in_arc = i;
+    }
+
+public:
     template <std::convertible_to<W> T>
     arc create_arc(const vertex from, const vertex to, T && weight) noexcept {
         const int n = static_cast<int>(_arcs.size());
-        _arcs.emplace_back(from, to, _first_in_arc[to], _first_out_arc[from],
-                           weight);
+        _arcs.emplace_back(from, to, -1, -1, _first_in_arc[to],
+                           _first_out_arc[from], weight);
+        if(_first_in_arc[to] != -1)
+            _arcs[static_cast<std::size_t>(_first_in_arc[to])].prev_in_arc = n;
         _first_in_arc[to] = n;
+        if(_first_out_arc[from] != -1)
+            _arcs[static_cast<std::size_t>(_first_out_arc[from])].prev_in_arc =
+                n;
         _first_out_arc[from] = n;
         return _arcs.back();
     }
-    // void remove_arc(const arc & uv) noexcept {
-    //     const vertex u = uv->first;
-    //     if(_adjacency_list[u].size() > 1)
-    //         std::swap(*uv, _adjacency_list[u].back());
-    //     _adjacency_list[u].pop_back();
-    // }
-    // void charge_target(arc & a, const vertex v) noexcept { a->first = v;
-    // }
+    void remove_arc(arc & a) noexcept {
+        const int i = index_of_arc(a);
+        remove_from_source_out_arcs(a);
+        remove_from_target_in_arcs(a);
+        std::swap(_arcs[static_cast<std::size_t>(i)], _arcs.back());
+        _arcs.pop_back();
+        change_arc_index(_arcs[i], i);
+    }
+    void charge_target(arc & a, const vertex v) noexcept {
+        if(a.target == v) return;
+        const int i = index_of_arc(a);
+        remove_from_target_in_arcs(a);
+        a.target = v;
+        a.prev_in_arc = -1;
+        a.next_in_arc = _first_in_arc[v];
+        if(_first_in_arc[v] != -1)
+            _arcs[static_cast<std::size_t>(_first_in_arc[v])].prev_in_arc = i;
+        _first_in_arc[v] = i;
+    }
+    void charge_source(arc & a, const vertex v) noexcept {
+        if(a.source == v) return;
+        const int i = index_of_arc(a);
+        remove_from_source_out_arcs(a);
+        a.source = v;
+        a.prev_out_arc = -1;
+        a.next_out_arc = _first_out_arc[v];
+        if(_first_out_arc[v] != -1)
+            _arcs[static_cast<std::size_t>(_first_out_arc[v])].prev_out_arc = i;
+        _first_out_arc[v] = i;
+    }
 
-    // template <typename T>
-    // static_map<vertex, T> create_vertex_map() const noexcept {
-    //     return static_map<vertex, T>(nb_vertices());
-    // }
-    // template <typename T>
-    // static_map<vertex, T> create_vertex_map(
-    //     const T & default_value) const noexcept {
-    //     return static_map<vertex, T>(nb_vertices(), default_value);
-    // }
+    template <typename T>
+    static_map<vertex, T> create_vertex_map() const noexcept {
+        return static_map<vertex, T>(nb_vertices());
+    }
+    template <typename T>
+    static_map<vertex, T> create_vertex_map(
+        const T & default_value) const noexcept {
+        return static_map<vertex, T>(nb_vertices(), default_value);
+    }
 };
 
 }  // namespace melon
