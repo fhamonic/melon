fead81ab884d2d7aa6791eaec30d8baa3c754afe
fhamonic <francois.hamonic@gmail.com>
2022-12-04 17:52:15 +0100

    graph modification concepts

--- test/static_digraph_test.cpp
+++ test/static_digraph_test.cpp
@@ -61,8 +61,8 @@ GTEST_TEST(static_digraph, vectors_constructor_1) {
                          std::ranges::views::values(arc_pairs));
     ASSERT_EQ(graph.nb_vertices(), 3);
     ASSERT_EQ(graph.nb_arcs(), 5);
-    ASSERT_TRUE(EQ_SETS(graph.vertices(), {0, 1, 2}));
-    ASSERT_TRUE(EQ_SETS(graph.arcs(), {0, 1, 2, 3, 4}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.vertices(), {0, 1, 2}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.arcs(), {0, 1, 2, 3, 4}));
 
     for(auto u : graph.vertices()) ASSERT_TRUE(graph.is_valid_vertex(u));
     ASSERT_FALSE(
@@ -71,15 +71,15 @@ GTEST_TEST(static_digraph, vectors_constructor_1) {
     for(auto a : graph.arcs()) ASSERT_TRUE(graph.is_valid_arc(a));
     ASSERT_FALSE(graph.is_valid_arc(arc_t<static_digraph>(graph.nb_arcs())));
 
-    ASSERT_TRUE(EQ_SETS(graph.out_neighbors(0), {1, 2}));
-    ASSERT_TRUE(EQ_SETS(graph.out_neighbors(1), {2}));
-    ASSERT_TRUE(EQ_SETS(graph.out_neighbors(2), {0, 1}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.out_neighbors(0), {1, 2}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.out_neighbors(1), {2}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.out_neighbors(2), {0, 1}));
 
-    ASSERT_TRUE(EQ_SETS(graph.in_neighbors(0), {2}));
-    ASSERT_TRUE(EQ_SETS(graph.in_neighbors(1), {0, 2}));
-    ASSERT_TRUE(EQ_SETS(graph.in_neighbors(2), {0, 1}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.in_neighbors(0), {2}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.in_neighbors(1), {0, 2}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.in_neighbors(2), {0, 1}));
 
-    ASSERT_TRUE(EQ_SETS(graph.arcs_pairs(), arc_pairs));
+    ASSERT_TRUE(EQ_MULTISETS(graph.arcs_pairs(), arc_pairs));
 
     for(arc_t<static_digraph> a : graph.arcs()) {
         ASSERT_EQ(graph.source(a), arc_pairs[a].first);
@@ -104,8 +104,8 @@ GTEST_TEST(static_digraph, vectors_constructor_2) {
     ASSERT_EQ(graph.nb_vertices(), 8);
     ASSERT_EQ(graph.nb_arcs(), 9);
 
-    ASSERT_TRUE(EQ_SETS(graph.vertices(), {0, 1, 2, 3, 4, 5, 6, 7}));
-    ASSERT_TRUE(EQ_SETS(graph.arcs(), {0, 1, 2, 3, 4, 5, 6, 7, 8}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.vertices(), {0, 1, 2, 3, 4, 5, 6, 7}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.arcs(), {0, 1, 2, 3, 4, 5, 6, 7, 8}));
 
     for(auto u : graph.vertices()) ASSERT_TRUE(graph.is_valid_vertex(u));
     ASSERT_FALSE(
@@ -115,12 +115,12 @@ GTEST_TEST(static_digraph, vectors_constructor_2) {
     ASSERT_FALSE(graph.is_valid_arc(arc_t<static_digraph>(graph.nb_arcs())));
 
     ASSERT_TRUE(EMPTY(graph.out_neighbors(0)));
-    ASSERT_TRUE(EQ_SETS(graph.out_neighbors(1), {2, 6, 7}));
-    ASSERT_TRUE(EQ_SETS(graph.out_neighbors(2), {3, 4}));
-    ASSERT_TRUE(EQ_SETS(graph.out_neighbors(6), {5}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.out_neighbors(1), {2, 6, 7}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.out_neighbors(2), {3, 4}));
+    ASSERT_TRUE(EQ_MULTISETS(graph.out_neighbors(6), {5}));
     ASSERT_TRUE(EMPTY(graph.out_neighbors(7)));
 
-    ASSERT_TRUE(EQ_SETS(graph.arcs_pairs(), arc_pairs));
+    ASSERT_TRUE(EQ_MULTISETS(graph.arcs_pairs(), arc_pairs));
 
     for(arc_t<static_digraph> a : graph.arcs()) {
         ASSERT_EQ(graph.source(a), arc_pairs[a].first);
