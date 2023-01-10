62032a2cbcf91c3e1eb2f5f2c7a159850bc09f25
= <francois.hamonic@gmail.com>
2023-01-10 18:29:45 +0100

    fuzzy test for strong_fiber algorithm

--- test/erdos_renyi_test.cpp
+++ test/erdos_renyi_test.cpp
@@ -7,15 +7,15 @@
 using namespace fhamonic::melon;
 
 GTEST_TEST(erdos_renyi, test) {
-    auto random_graph1 = erdos_renyi(100, 0.0);
+    auto random_graph1 = erdos_renyi<static_digraph>(100, 0.0);
     ASSERT_EQ(random_graph1.nb_vertices(), 100);
     ASSERT_EQ(random_graph1.nb_arcs(), 0);
 
-    auto random_graph2 = erdos_renyi(100, 1.0);
+    auto random_graph2 = erdos_renyi<static_digraph>(100, 1.0);
     ASSERT_EQ(random_graph2.nb_vertices(), 100);
     ASSERT_EQ(random_graph2.nb_arcs(), 9900);
 
-    auto random_graph3 = erdos_renyi(100, 0.5);
+    auto random_graph3 = erdos_renyi<static_digraph>(100, 0.5);
     ASSERT_EQ(random_graph3.nb_vertices(), 100);
     // the probability of the two following tests to fail is 2*10^-60,
     // a billion times more than the number of atoms on Earth...
