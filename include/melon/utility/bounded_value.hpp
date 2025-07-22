78f0460f2a05cd7369ea13a4e9390dbb5f2b5e85
fhamonic <francois.hamonic@gmail.com>
2025-07-22 04:33:16 +0200

    new bounded value promotion strategy mechanism

--- include/melon/utility/bounded_value.hpp
+++ include/melon/utility/bounded_value.hpp
@@ -12,8 +12,40 @@
 namespace fhamonic {
 namespace melon {
 
-template <typename T1, typename T2>
-using larger_type_t = std::conditional_t<sizeof(T1) < sizeof(T2), T2, T1>;
+namespace detail {
+
+template <typename... Ts>
+struct type_list {};
+
+template <template <typename> typename Pred, typename TList>
+struct first_matching;
+
+template <template <typename> typename Pred, typename First, typename... Rest>
+struct first_matching<Pred, type_list<First, Rest...>> {
+    using type = std::conditional_t<
+        Pred<First>::value, First,
+        typename first_matching<Pred, type_list<Rest...>>::type>;
+};
+
+template <template <typename> typename Pred>
+struct first_matching<Pred, type_list<>> {
+    using type = void;
+};
+
+template <template <typename> typename Pred, typename TList>
+using first_matching_t = first_matching<Pred, TList>::type;
+
+template <typename From, typename To>
+concept narrowing_conversion = !requires(From f) { To{f}; };
+
+template <typename From, typename To>
+concept non_narrowing = !narrowing_conversion<From, To>;
+
+template <typename A, typename B>
+    requires(non_narrowing<A, B> || non_narrowing<B, A>)
+using common_number_type = std::conditional_t<non_narrowing<A, B>, B, A>;
+
+}  // namespace detail
 
 // clang-format off
 template <typename _Traits, typename T>
@@ -21,44 +53,75 @@ concept promotion_strategy = requires(const T & v) {
     { _Traits::plus_overflows(v, v) } -> std::convertible_to<bool>;
     { _Traits::substract_overflows(v, v) } -> std::convertible_to<bool>;
     { _Traits::multiply_overflows(v, v) } -> std::convertible_to<bool>;
-    { v } -> std::convertible_to<typename _Traits::promoted_type<T>>;
 };
 // clang-format on
 
 // Default promotion strategy for integer types
 struct default_promotion_strategy {
-    template <typename T>
-    using promoted_type = std::conditional_t<
-        std::same_as<T, int8_t>, int16_t,
-        std::conditional_t<
-            std::same_as<T, int16_t>, int32_t,
-            std::conditional_t<std::same_as<T, int32_t>, int64_t,
-                               std::conditional_t<std::same_as<T, int64_t>,
-                                                  __int128_t, void>>>>;
+    using type_hierarchy = detail::type_list<int8_t, int16_t, int32_t, int64_t>;
 
     template <typename A, typename B>
     static constexpr bool plus_overflows(const A & a, const B & b) {
-        using T = larger_type_t<A, B>;
+        using T = detail::common_number_type<A, B>;
         return (b > 0 && a > std::numeric_limits<T>::max() - b) ||
                (b < 0 && a < std::numeric_limits<T>::min() - b);
     }
 
     template <typename A, typename B>
     static constexpr bool substract_overflows(const A & a, const B & b) {
-        using T = larger_type_t<A, B>;
+        using T = detail::common_number_type<A, B>;
         return (b > 0 && a < std::numeric_limits<T>::min() + b) ||
                (b < 0 && a > std::numeric_limits<T>::max() + b);
     }
 
     template <typename A, typename B>
     static constexpr bool multiply_overflows(const A & a, const B & b) {
-        using T = larger_type_t<A, B>;
+        using T = detail::common_number_type<A, B>;
         if(a == 0 || b == 0) return false;
         if(a > 0 && b > 0) return a > std::numeric_limits<T>::max() / b;
         if(a < 0 && b < 0) return a < std::numeric_limits<T>::max() / b;
         if(a > 0 && b < 0) return b < std::numeric_limits<T>::min() / a;
         return a < std::numeric_limits<T>::min() / b;
     }
+
+    template <typename A, A A_MIN, A A_MAX, typename B, B B_MIN, B B_MAX>
+    struct predicates {
+        template <typename T>
+        struct can_hold_plus {
+            static constexpr bool value = false;
+        };
+        template <typename T>
+            requires(detail::non_narrowing<A, T> && detail::non_narrowing<B, T>)
+        struct can_hold_plus<T> {
+            static constexpr bool value =
+                !(plus_overflows(T{A_MIN}, T{B_MIN}) ||
+                  plus_overflows(T{A_MAX}, T{B_MAX}));
+        };
+        template <typename T>
+        struct can_hold_substract {
+            static constexpr bool value = false;
+        };
+        template <typename T>
+            requires(detail::non_narrowing<A, T> && detail::non_narrowing<B, T>)
+        struct can_hold_substract<T> {
+            static constexpr bool value =
+                !(substract_overflows(T{A_MIN}, T{B_MAX}) ||
+                  substract_overflows(T{A_MAX}, T{B_MIN}));
+        };
+        template <typename T>
+        struct can_hold_multiply {
+            static constexpr bool value = false;
+        };
+        template <typename T>
+            requires(detail::non_narrowing<A, T> && detail::non_narrowing<B, T>)
+        struct can_hold_multiply<T> {
+            static constexpr bool value =
+                !(multiply_overflows(T{A_MIN}, T{B_MIN}) ||
+                  multiply_overflows(T{A_MIN}, T{B_MAX}) ||
+                  multiply_overflows(T{A_MAX}, T{B_MIN}) ||
+                  multiply_overflows(T{A_MAX}, T{B_MAX}));
+        };
+    };
 };
 
 template <typename T, T _MIN, T _MAX, promotion_strategy<T> _PS>
@@ -129,11 +192,10 @@ template <typename T1, T1 MIN1, T1 MAX1, typename PS1, typename T2, T2 MIN2,
     requires std::same_as<PS1, PS2>
 constexpr auto operator+(const bounded_value<T1, MIN1, MAX1, PS1> & a,
                          const bounded_value<T2, MIN2, MAX2, PS2> & b) {
-    using return_value_type =
-        std::conditional_t<PS1::plus_overflows(MIN1, MIN2) ||
-                               PS1::plus_overflows(MAX1, MAX2),
-                           typename PS1::promoted_type<larger_type_t<T1, T2>>,
-                           larger_type_t<T1, T2>>;
+    using return_value_type = detail::first_matching_t<
+        PS1::template predicates<T1, MIN1, MAX1, T2, MIN2,
+                                 MAX2>::template can_hold_plus,
+        typename PS1::type_hierarchy>;
     return bounded_value<
         return_value_type, return_value_type{MIN1} + return_value_type{MIN2},
         return_value_type{MAX1} + return_value_type{MAX2}, PS1>(
@@ -145,11 +207,10 @@ template <typename T1, T1 MIN1, T1 MAX1, typename PS1, typename T2, T2 MIN2,
     requires std::same_as<PS1, PS2>
 constexpr auto operator-(const bounded_value<T1, MIN1, MAX1, PS1> & a,
                          const bounded_value<T2, MIN2, MAX2, PS2> & b) {
-    using return_value_type =
-        std::conditional_t<PS1::substract_overflows(MIN1, MAX2) ||
-                               PS1::substract_overflows(MAX1, MIN2),
-                           typename PS1::promoted_type<larger_type_t<T1, T2>>,
-                           larger_type_t<T1, T2>>;
+    using return_value_type = detail::first_matching_t<
+        PS1::template predicates<T1, MIN1, MAX1, T2, MIN2,
+                                 MAX2>::template can_hold_substract,
+        typename PS1::type_hierarchy>;
     return bounded_value<
         return_value_type, return_value_type{MIN1} - return_value_type{MAX2},
         return_value_type{MAX1} - return_value_type{MIN2}, PS1>(
@@ -161,13 +222,10 @@ template <typename T1, T1 MIN1, T1 MAX1, typename PS1, typename T2, T2 MIN2,
     requires std::same_as<PS1, PS2>
 constexpr auto operator*(const bounded_value<T1, MIN1, MAX1, PS1> & a,
                          const bounded_value<T2, MIN2, MAX2, PS2> & b) {
-    using return_value_type =
-        std::conditional_t<PS1::multiply_overflows(MIN1, MIN2) ||
-                               PS1::multiply_overflows(MIN1, MAX2) ||
-                               PS1::multiply_overflows(MAX1, MIN2) ||
-                               PS1::multiply_overflows(MAX1, MAX2),
-                           typename PS1::promoted_type<larger_type_t<T1, T2>>,
-                           larger_type_t<T1, T2>>;
+    using return_value_type = detail::first_matching_t<
+        PS1::template predicates<T1, MIN1, MAX1, T2, MIN2,
+                                 MAX2>::template can_hold_multiply,
+        typename PS1::type_hierarchy>;
     return bounded_value<
         return_value_type,
         std::min({return_value_type{MIN1} * return_value_type{MIN2},
@@ -250,7 +308,7 @@ public:
     using value_type = T;
     using promotion_strategy_t = _PS;
 
-    constexpr bounded_value(T v) {};
+    constexpr bounded_value(T v) {}
     constexpr bounded_value() = default;
     constexpr bounded_value(const bounded_value &) = default;
     constexpr bounded_value(bounded_value &&) = default;
