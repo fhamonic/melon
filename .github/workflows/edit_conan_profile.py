c2fc91710fa777f19ec11d992b6c762cb813c528
fhamonic <francois.hamonic@gmail.com>
2025-11-05 21:18:15 +0100

    workflow

--- .github/workflows/edit_conan_profile.py
+++ .github/workflows/edit_conan_profile.py
@@ -5,11 +5,9 @@ if len(sys.argv) < 2:
 
 profile_path = sys.argv[1]
 
-print(profile_path)
-
 profile = dict()
 current_section = ""
-profile[current_section] = dict()
+profile[current_section] = (dict(),list())
 
 with open(profile_path, "r") as f:
     for line in f.readlines():
@@ -18,21 +16,30 @@ with open(profile_path, "r") as f:
         if stripped_line[0] == '#': continue
         if stripped_line[0] == '[':
             current_section=stripped_line[stripped_line.find('[')+1:stripped_line.find(']')]
-            profile[current_section] = dict()
+            profile[current_section] = (dict(),list())
             continue
-        setting, value = stripped_line.split('=')
-        profile[current_section][setting] = value
+        if "=" in stripped_line:
+            setting, value = stripped_line.split('=')
+            profile[current_section][0][setting] = value
+        else:
+            profile[current_section][1].append(stripped_line)
 
 for arg in sys.argv[2:]:
-    section = arg[0:arg.find('.')]
-    setting, value = arg[arg.find('.')+1:].split('=')
+    section = arg[0:arg.find(':')]
     if section not in profile:
-        profile[section] = dict()
-    profile[section][setting] = value
+        profile[section] = (dict(),list())
+    stripped_line = arg[arg.find(':')+1:]
+    if "=" in stripped_line:
+        setting, value = stripped_line.split('=')
+        profile[current_section][0][setting] = value
+    else:
+        profile[current_section][1].append(stripped_line)
 
 with open(profile_path, "w") as f:
     for section, settings in profile.items():
         if section != "":
             f.write('\n['+section+"]\n")
-        for setting, value in settings.items():
+        for setting, value in settings[0].items():
             f.write(setting+'='+value+'\n')
+        for value in settings[1].items():
+            f.write(value+'\n')
