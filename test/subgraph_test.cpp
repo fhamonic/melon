ee529d05fed61f9fb8ef91cafb079981bc244129
fhamonic <francois.hamonic@gmail.com>
2025-11-12 00:19:29 +0100

    added induced_sugraph

--- test/subgraph_test.cpp
+++ test/subgraph_test.cpp
@@ -19,9 +19,8 @@ GTEST_TEST(subgraph_views, static_graph) {
         arc_pairs(
             {{0, {0, 1}}, {1, {0, 2}}, {2, {1, 2}}, {3, {2, 0}}, {4, {2, 1}}});
 
-    static_digraph graph(
-        3, std::views::keys(std::views::values(arc_pairs)),
-        std::views::values(std::views::values(arc_pairs)));
+    static_digraph graph(3, std::views::keys(std::views::values(arc_pairs)),
+                         std::views::values(std::views::values(arc_pairs)));
 
     auto subgraph_graph = views::subgraph(graph);
 
@@ -155,6 +154,51 @@ GTEST_TEST(subgraph_views, dijkstra) {
     alg.advance();
     ASSERT_FALSE(alg.finished());
     ASSERT_EQ(alg.current(), std::make_pair(3u, 21));
+    alg.advance();
+    ASSERT_TRUE(alg.finished());
+}
+
+GTEST_TEST(induces_subgraph_views, test) {
+    static_digraph_builder<static_digraph, int> builder(6);
+
+    builder.add_arc(0, 1, 7);
+    builder.add_arc(0, 2, 9);  //
+    builder.add_arc(0, 5, 14);
+    builder.add_arc(1, 0, 7);
+    builder.add_arc(1, 2, 10);  //
+    builder.add_arc(1, 3, 15);  //
+    builder.add_arc(2, 0, 9);   //
+    builder.add_arc(2, 1, 10);  //
+    builder.add_arc(2, 3, 12);  //
+    builder.add_arc(2, 5, 2);   //
+    builder.add_arc(3, 1, 15);  //
+    builder.add_arc(3, 2, 12);  //
+    builder.add_arc(3, 4, 6);   //
+    builder.add_arc(4, 3, 6);   //
+    builder.add_arc(4, 5, 9);
+    builder.add_arc(5, 0, 14);
+    builder.add_arc(5, 2, 2);  //
+    builder.add_arc(5, 4, 9);
+
+    auto [fgraph, length_map] = builder.build();
+    auto graph = views::induced_subgraph(
+        fgraph,
+        std::views::concat(std::views::iota(0u, 2u), std::views::iota(4u, 6u)));
+
+    auto alg = dijkstra(graph, length_map);
+
+    alg.add_source(0u);
+    ASSERT_FALSE(alg.finished());
+    ASSERT_EQ(alg.current(), std::make_pair(0u, 0));
+    alg.advance();
+    ASSERT_FALSE(alg.finished());
+    ASSERT_EQ(alg.current(), std::make_pair(1u, 7));
+    alg.advance();
+    ASSERT_FALSE(alg.finished());
+    ASSERT_EQ(alg.current(), std::make_pair(5u, 14));
+    alg.advance();
+    ASSERT_FALSE(alg.finished());
+    ASSERT_EQ(alg.current(), std::make_pair(4u, 23));
     alg.advance();
     ASSERT_TRUE(alg.finished());
 }
